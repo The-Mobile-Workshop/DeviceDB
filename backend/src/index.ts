6419230f2dfc0ec6ba8837acbdaf280e32a253d0
Laith-GLaDOS <usr.bin.ducc.2@gmail.com>
2022-10-17 23:02:10 +0300

    add cors to backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Response } from 'express';
 import db from './utils/db';
 import router from './router';
 
@@ -9,6 +9,12 @@ const port = process.env.PORT || 8080;
 const server = express();
 
 server.use(express.json());
+server.use((_: any, res: Response, next: Function) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Methods', '*');
+  next();
+});
 server.use(router);
 
 server.listen(port, () => console.log(`Started server at 0.0.0.0:${port}`));
